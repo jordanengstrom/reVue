d57fbbfa469c6ea4322a4e833165baa7fe6a0dbd
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-12-24 16:56:27 -0700

    made Vue instance, reviewed v-bind and v-text directives

---
+++ main.js
@@ -0,0 +1,9 @@
+const app= new Vue({
+  el: "#app",
+  data: {
+    message: "Hello world!",
+    value: 5,
+    imgSrc: "https://t9b8t3v6.rocketcdn.me/wp-content/uploads/2017/10/tuxedo-cat-names-5.jpg",
+    link: "https://vuejs.org/"
+  }
+})
