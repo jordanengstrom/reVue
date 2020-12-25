8d4a99933967ae86bebd28f2e5ecd613a6c6d75b
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-12-24 19:35:04 -0700

    covered list rendering

---
+++ list_rendering/main.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    users: [{
+      id: 567,
+      name: "alice",
+      profession: "developer"
+    },
+    {
+      id: 568,
+      name: "bob",
+      profession: "manager"
+    },
+    {
+      id: 569,
+      name: "jordan",
+      profession: "developer"
+    },
+    {
+      id: 570,
+      name:  "goose",
+      profession: "designer"
+    },
+    {
+      id: 571,
+      name: "benny",
+      profession: "manager"
+    }]
+  },
+  methods: {}
+})
+
+
