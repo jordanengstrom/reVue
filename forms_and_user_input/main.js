24048c580270f8b94efde71a72b19e0c82c8c1f9
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-12-26 13:52:34 -0700

    re-did project to make a comments application

--- forms_and_user_input/main.js
+++ forms_and_user_input/main.js
@@ -1,11 +1,25 @@
 const app = new Vue({
   el: "#app",
   data: {
-    text: '',
-    checked: true,
-    city: ''
+    comment: null,
+    comments: [],
+    errors: null
   },
   methods: {
+    onSubmit() {
+      if (this.comment) {
+        let newComment = this.comment
+        this.comments.push(newComment);
+        this.comment = null;
 
+        if (this.errors) {
+          this.errors = null;
+        }
+
+      } else {
+        this.errors = "This comment field can't be empty"
+      }
+
+    }
   }
 })
