a27731942dace681bd51f6f7d34fabc97050c2f8
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-12-30 21:30:21 -0700

    added single-task and task-list components

--- to_do_app_assessment/main.js
+++ to_do_app_assessment/main.js
@@ -1,16 +1,51 @@
-Vue.component("task", {
+const taskList = Vue.component("task-list", {
   props: {
-    comment: {
-      type: Object,
+    tasks: {
+      type: Array,
       required: true
+    }
+  },
+
+  // data: function () {
+  //   return {
+  //     newTask: null,
+  //     error: null
+  //   }
+  // },
+  template: `
+  <div>
+    <single-task v-for="(task, index) in tasks" :task="task" :key="index"></single-task>
+  </div>
+  `
+});
+
+const singleTask = Vue.component("single-task", {
+  props: {
+    task: {
+      type: String,
+      required: false
+    }
+  },
+  methods: {
+    deleteTask(clickedTask) {
+      if (clickedTask) {
+        let taskIndex = this.tasks.indexOf(clickedTask);
+        if (taskIndex > -1) {
+          this.tasks.splice(taskIndex, 1);
+        }
+      }
     }
   },
   template: `
     <div>
-      <div class="card-body">
-        <p>{{ task }}</p>
+      <div class="alert alert-success alert-dismissible fade show mb-2" role="alert">
+        {{ task }}
+        <button type="button" @click="deleteTask" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-    </div>`
+    </div>
+    `
 });
 
 const app = new Vue({
@@ -21,9 +56,9 @@ const app = new Vue({
     error: "To add a new task, write something and press enter!"
   },
   methods: {
-    submitTask(taskString) {
+    submitTask() {
       if (this.task) {
-        let newTask = this.task
+        let newTask = this.task;
         this.tasks.push(newTask);
         this.task = null;
         if (this.error) {
@@ -32,20 +67,16 @@ const app = new Vue({
       } else {
         this.error = "You must add a task before submitting."
       }
-    },
-    deleteTask(clickedTask) {
-      if (clickedTask) {
-        let taskIndex = this.tasks.indexOf(clickedTask);
-        if (taskIndex > -1) {
-          this.tasks.splice(taskIndex, 1);
-        }
-      }
     }
   },
   computed: {
     getTaskCount() {
       return this.tasks.length;
     }
+  },
+  components: {
+    task: singleTask,
+    taskList: taskList
   }
 
 });
